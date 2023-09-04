e9624ed1b61bf40bb2a0c4b36142e8f76a2e7438
Ilia Burykin <96836606+ZotIL2@users.noreply.github.com>
2023-09-04 21:17:18 +0300

    secondchange
    new print

--- src/App.java
+++ src/App.java
@@ -1,4 +1,5 @@
 class Lab1 {
     public static void main(String[] args)
-     {System.out.println("Its wotks");}
+     {System.out.println("Its wotks");
+    System.out.println("Git hub hello");}
 }
