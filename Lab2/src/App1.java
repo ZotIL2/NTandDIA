90e872ae17da27e45163882419de282c6010d9d9
Ilia Burykin <96836606+ZotIL2@users.noreply.github.com>
2023-09-05 10:56:53 +0300

    App2

--- Lab2/src/App1.java
+++ Lab2/src/App1.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 class Lab1 {
-    public static void main(String[] args)
-     {System.out.println("Its wotks");
-    System.out.println("Git hub hello");}
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input a number: ");
+        int levelCount = in.nextInt();
+        printPineTree(levelCount);
+        in.close();
+    }
+
+    private static void printPineTree(int levelCount) {
+        for (int i = 0; i < levelCount; i++) {
+            for (int j = 0; j < levelCount - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 0; k <= i; k++) {
+                System.out.print("*");
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
 }
---
+++ Lab2/src/App2.java
@@ -0,0 +1,28 @@
+public class App2 {
+    public static void main(String[] args) {
+        int rows = 5; 
+        int cols = 5; 
+        int[][] array = ArrayCreate(rows, cols);
+
+        // Виведення масиву на екран
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    // Метод для створення і заповнення двовимірного масиву
+    public static int[][] ArrayCreate(int rows, int cols) {
+        int[][] array = new int[rows][cols];
+        int startV = 1; 
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                array[i][j] = startV;
+                startV += 3; // Кожен наступний елемент на 3 більший
+            }
+        }
+        return array;
+    }
+}
